9e94ff00ebbf892fdb8193f607d833ada255c51b
MaloLebrin <malolebrin@gmail.com>
2025-09-01 09:45:14 +0200

    core: export createEntityProxy and EntityMeta/EntityWithMeta for external usage

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,10 +2,12 @@
 export { default as createActions } from './createActions.js';
 export { default as createGetters } from './createGetters.js';
 export { default as createState } from './createState.js';
+export { createEntityProxy } from './utils/createEntityProxy.js';
 
 // Export types for other packages to use
 export type { State } from './types/State.js';
 export type { WithId } from './types/WithId.js';
 export type { FilterFn, OptionalFilterFn } from './types/Filter.js';
 export type { ByIdParams } from './types/ByIdParams.js';
+export type { EntityMeta, EntityWithMeta } from './types/EntityMeta.js';
 
